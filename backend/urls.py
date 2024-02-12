f86653933c9a9a7860a58a1b643ef1e30422e395
phsaikiran <saikiran.2310@gmail.com>
2024-02-12 16:44:29 -0600

    Using inbuild auth, login registration pages

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('user', views.get_all_users, name='Get all users'),
     path('user/create', views.user_create, name='Create user'),
     path('user/<str:pk>', views.get_user, name='Get user by ID'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from backend.models import User
+from .serializers import UserSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+@login_required
+@api_view(['GET'])
+def get_all_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@login_required
+@api_view(['GET'])
+def get_user(request, pk):
+    try:
+        user = User.objects.get(id=pk)
+    except User.DoesNotExist:
+        return Response({'message': 'User not found'}, status=404)
+
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@login_required
+@api_view(['POST'])
+def user_create(request):
+    # Hash the password before saving it to the database
+    request.data['password'] = make_password(request.data['password'])
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    else:
+        return Response(serializer.errors, status=400)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
